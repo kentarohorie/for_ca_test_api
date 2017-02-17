2a872771f3a99d97d9aff8fdff5e02423fc4eeff
kentarohorie <kentaro.horie.4@gmail.com>
2017-02-17 14:29:57 +0900

    implemetn

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,17 +1,53 @@
 class ResponsesController < ApplicationController
   def series
-    if params[:op] == "or"
-      @series = Series.where("name like '%#{params[:name]}%' OR casts like '%#{params[:casts]}%' OR crews like '%#{params[:crews]}%'")
+    if params[:op] != "or" and params[:op] != "and"
+      @response = {
+        status: 400
+      }
+      render json: @response
     else
-      @series = Series.where("name like '%#{params[:name]}%' AND casts like '%#{params[:casts]}%' AND crews like '%#{params[:crews]}%'")
+      if params[:op] == "or"
+        @series = Series.where("name like '%#{params[:name]}%' OR casts like '%#{params[:casts]}%' OR crews like '%#{params[:crews]}%'")
+      else
+        @series = Series.where("name like '%#{params[:name]}%' AND casts like '%#{params[:casts]}%' AND crews like '%#{params[:crews]}%'")
+      end
+        @series.order("name ASC")
+        render json: @series
     end
-    @series.order("name ASC")
-    render json: @series
   end
 
   def channel_schedules
   end
 
   def channel_schedules_show
+    @cs = ChannelSchedule.find_by_id(params[:id])
+    if @cs == nil
+      @response = {
+        status: 404
+      }
+    elsif params[:exclude] != "comments" and params[:exclude] != nil
+      @response = {
+        status: 400
+      }
+    else
+      @pg = @cs.program
+      @comments = @cs.comments
+      @response = {
+        id: @cs.id,
+        channel: @cs.channel,
+        startAt: @cs.start_at,
+        endAt: @cs.end_at,
+        program: {
+          title: @pg.title,
+          content: @pg.content,
+          highlight: @pg.highlight,
+          casts: @pg.casts,
+          crews: @pg.crews
+        },
+        comments: @comments
+      }
+      @response.delete(:comments)  if params[:exclude] == "comments"
+    end
+    render json: @response
   end
 end
